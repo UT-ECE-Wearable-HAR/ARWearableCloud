bafd37a8d2b51c4bf823fc9cce366c30798c7421
Tony <tonyli0409@utexas.edu>
2021-03-07 14:32:41 -0600

    use new python bindings, set correct datatypes for mpu

--- api/socketthread.py
+++ api/socketthread.py
@@ -24,80 +24,73 @@ def compress_mpudict(dict):
         "accel":accel, "linaccel":linaccel, "linaccelinworld":linaccelinworld, "euler":euler}
 
 def mpu_extract_10(packet):
-    result = {point: np.array([]) for point in ["qw","qx","qy","qz","gravx","gravy","gravz","y","p","r","ax","ay","az",
-        "gyrx","gyry","gyrz","linaccelx","linaccely","linaccelz","lawx","lawy","lawz","ex","ey","ez"]}
-
+    result = {point: np.array([], dtype='float32') for point in ["qw","qx","qy","qz","gravx","gravy","gravz",
+        "y","p","r","ex","ey","ez"]}
+    result.update({point: np.array([], dtype='int16') for point in ["ax","ay","az","gyrx","gyry","gyrz",
+        "linaccelx","linaccely","linaccelz","lawx","lawy","lawz"]})
     for idx in range(10):
         entry = packet[:42]
         packet = packet[42:]
 
-        q = dmp.Quaternion()
-        dmp.dmpGetQuaternion(q, entry)
-        result['qw'] = np.append(result['qw'], q.w)
-        result['qx'] = np.append(result['qx'], q.x)
-        result['qy'] = np.append(result['qy'], q.y)
-        result['qz'] = np.append(result['qz'], q.z)
+        q = dmp.quaternion(packet)
+        result['qw'] = np.append(result['qw'], q[0])
+        result['qx'] = np.append(result['qx'], q[1])
+        result['qy'] = np.append(result['qy'], q[2])
+        result['qz'] = np.append(result['qz'], q[3])
         #logger.info(result['qw'])
         #logger.info(result['qx'])
         #logger.info(result['qy'])
         #logger.info(result['qz'])
         #logger.info(f"quaternion: w: {q.w}, x: {q.x}, y: {q.y}, z: {q.z}")
 
-        gravity = dmp.VectorFloat()
-        dmp.dmpGetGravity(gravity, q)
-        result['gravx'] = np.append(result['gravx'], gravity.x)
-        result['gravy'] = np.append(result['gravy'], gravity.y)
-        result['gravz'] = np.append(result['gravz'], gravity.z)
+        gravity = dmp.gravity(packet)
+        result['gravx'] = np.append(result['gravx'], gravity[0])
+        result['gravy'] = np.append(result['gravy'], gravity[1])
+        result['gravz'] = np.append(result['gravz'], gravity[2])
         #logger.info(f"gravity: x: {gravity.x}, y: {gravity.y}, z: {gravity.z}")
 
-        ypr = dmp.VectorFloat()
-        dmp.dmpGetYawPitchRoll(ypr, q, gravity)
-        result['y'] = np.append(result['y'], ypr.z)
-        result['p'] = np.append(result['p'], ypr.y)
-        result['r'] = np.append(result['r'], ypr.x)
+        ypr = dmp.yawPitchRoll(packet)
+        result['y'] = np.append(result['y'], ypr[0])
+        result['p'] = np.append(result['p'], ypr[1])
+        result['r'] = np.append(result['r'], ypr[2])
         # ypr is radians, converted here to degrees for display
         #logger.info("YAW: %3.1f" % (ypr.z * 180 / math.pi))
         #logger.info("PITCH: %3.1f" % (ypr.y * 180 / math.pi))
         #logger.info("ROLL: %3.1f" % (ypr.x * 180 / math.pi))
 
         
-        vec = dmp.VectorInt16()
-        dmp.dmpGetGyro(vec, entry)
-        result['gyrx'] = np.append(result['gyrx'], vec.x)
-        result['gyry'] = np.append(result['gyry'], vec.y)
-        result['gyrz'] = np.append(result['gyrz'], vec.z)
+        gyro = dmp.gyro(packet)
+        result['gyrx'] = np.append(result['gyrx'], gyro[0])
+        result['gyry'] = np.append(result['gyry'], gyro[1])
+        result['gyrz'] = np.append(result['gyrz'], gyro[2])
         #logger.info(f"Gyro: x: {vec.x}, y: {vec.y}, z: {vec.z}")
 
 
-        accel = dmp.VectorInt16()
-        dmp.dmpGetAccel(accel, entry)
-        result['ax'] = np.append(result['ax'], accel.x)
-        result['ay'] = np.append(result['ay'], accel.y)
-        result['az'] = np.append(result['az'], accel.z)
+        accel = dmp.accel(packet)
+        result['ax'] = np.append(result['ax'], accel[0])
+        result['ay'] = np.append(result['ay'], accel[1])
+        result['az'] = np.append(result['az'], accel[2])
         #logger.info(f"Accel: x: {accel.x}, y: {accel.y}, z: {accel.z}")
 
         # remmoves gravity component from accel
-        accelReal = dmp.VectorInt16()
-        dmp.dmpGetLinearAccel(accelReal, accel, gravity)
-        result['linaccelx'] = np.append(result['linaccelx'], accelReal.x)
-        result['linaccely'] = np.append(result['linaccely'], accelReal.y)
-        result['linaccelz'] = np.append(result['linaccelz'], accelReal.z)
+        linAccel = dmp.linAccel(packet)
+        result['linaccelx'] = np.append(result['linaccelx'], linAccel[0])
+        result['linaccely'] = np.append(result['linaccely'], linAccel[1])
+        result['linaccelz'] = np.append(result['linaccelz'], linAccel[2])
         #logger.info(f"LinearAccel: x: {accelReal.x}, y: {accelReal.y}, z: {accelReal.z}")
 
         # rotate measured 3D acceleration vector into original state
         # frame of reference based on orientation quaternion
-        vec = dmp.VectorInt16()
-        dmp.dmpGetLinearAccelInWorld(vec, accelReal, q)
-        result['lawx'] = np.append(result['lawx'], vec.x)
-        result['lawy'] = np.append(result['lawy'], vec.y)
-        result['lawz'] = np.append(result['lawz'], vec.z)
+        linAccelWorld = dmp.linAccelWorld(packet)
+        result['lawx'] = np.append(result['lawx'], linAccelWorld[0])
+        result['lawy'] = np.append(result['lawy'], linAccelWorld[1])
+        result['lawz'] = np.append(result['lawz'], linAccelWorld[2])
         #logger.info(f"LinearAccelInWorld: x: {vec.x}, y: {vec.y}, z: {vec.z}")
 
-        euler = dmp.VectorFloat()
-        dmp.dmpGetEuler(euler, q)
-        result['ex'] = np.append(result['ex'], euler.x)
-        result['ey'] = np.append(result['ey'], euler.y)
-        result['ez'] = np.append(result['ez'], euler.z)
+        euler = dmp.euler(packet)
+        result['ex'] = np.append(result['ex'], euler[0])
+        result['ey'] = np.append(result['ey'], euler[1])
+        result['ez'] = np.append(result['ez'], euler[2])
         # euler angles are in radians
         #logger.info(f"euler: psi: {euler.x}, theta: {euler.y}, phi: {euler.z}")
         packet = packet[42:]
@@ -138,6 +131,12 @@ def socketthread(user):
             #logger.info(mpu_results)
             c.send("MPU_RECV\n".encode())
             userprofile = UserProfile.objects.get(pk=user)
+
+            #logger.info("quart" + str(mpu_results['quart'].dtype) + "gravity" + str(mpu_results['gravity'].dtype) + 
+                "ypr" + str(mpu_results['ypr'].dtype) + "gyro" + str(mpu_results['gyro'].dtype) + "accel" + str(mpu_results['accel'].dtype) + 
+                "linaccel" + str(mpu_results['linaccel'].dtype) + "law" + str(mpu_results['linaccelinworld'].dtype) + "euler" + str(mpu_results['euler'].dtype))
+
+
             data = DataCapture(user = userprofile, img = res[4:], quarternion = mpu_results['quart'], 
                 gravity = mpu_results['gravity'].tobytes(), ypr = mpu_results['ypr'].tobytes(), gyro = mpu_results['gyro'].tobytes(), 
                 accel = mpu_results['accel'].tobytes(), linaccel = mpu_results['linaccel'].tobytes(), 
